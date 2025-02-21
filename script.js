633ebafdaa95e98846c6843c816880ac91b76929
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-02-20 19:18:24 -0700

    Add playGame function logic and move playRound and score variables to be declared inside function

--- script.js
+++ script.js
@@ -22,35 +22,50 @@ function getHumanChoice() {
   }
 }
 
-let humanScore = 0
-let computerScore = 0
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log("TIE! No points awarded!")
-  } else if (humanChoice === "rock") {
-    if (computerChoice === "paper") {
-      console.log("You Lose! Paper covers Rock!")
-      computerScore++
+function playGame(rounds = 5) {
+  let humanScore = 0
+  let computerScore = 0
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("TIE! No points awarded!")
+    } else if (humanChoice === "rock") {
+      if (computerChoice === "paper") {
+        console.log("You Lose! Paper covers Rock!")
+        computerScore++
+      } else {
+        console.log("WINNER! Rock smashes Scissors!")
+        humanScore++
+      }
+    } else if (humanChoice === "paper") {
+      if (computerChoice === "scissors") {
+        console.log("You Lose! Scissors cuts Paper!")
+        computerScore++
+      } else {
+        console.log("WINNER! Paper covers Rock!")
+        humanScore++
+      }
     } else {
-      console.log("WINNER! Rock smashes Scissors!")
-      humanScore++
+      if (computerChoice === "rock") {
+        console.log("You Lose! Rock smashes Scissors!")
+        computerScore++
+      } else {
+        console.log("WINNER! Scissors cuts Paper!")
+        humanScore++
+      }
     }
-  } else if (humanChoice === "paper") {
-    if (computerChoice === "scissors") {
-      console.log("You Lose! Scissors cuts Paper!")
-      computerScore++
-    } else {
-      console.log("WINNER! Paper covers Rock!")
-      humanScore++
-    }
-  } else {
-    if (computerChoice === "rock") {
-      console.log("You Lose! Rock smashes Scissors!")
-      computerScore++
-    } else {
-      console.log("WINNER! Scissors cuts Paper!")
-      humanScore++
-    }
+  }
+  while (rounds > 0) {
+    playRound(getHumanChoice(), getComputerChoice())
+    rounds--
+  }
+  if (humanScore > computerScore) {
+    console.log(`WINNER! ${humanScore} to ${computerScore}`)
+  }
+  else if (computerScore > humanScore) {
+    console.log(`You Lose! ${computerScore} to ${humanScore}`)
+  }
+  else {
+    console.log(`TIE GAME!`)
   }
 }
