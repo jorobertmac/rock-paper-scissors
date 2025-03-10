607666a1b48f2a731391f3c3f2577a9955825897
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-03-10 15:12:37 -0700

    Make UI able to replay

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
-function playGame(rounds = 5) {
+function playGame(){
+  // variables
+  let playerSelection = null
   let playerScore = 0
   let computerScore = 0
-  let roundNumber = 0
-  let playerSelection = ""
-
+  let roundNumber = 1
+  
+  // querySelectors
   const makeSelectionText = document.querySelector("#playerSelection")
-  const selection = document.querySelectorAll(".selection")
-  const playButton = document.querySelector(".play")
+  const selections = document.querySelectorAll(".selection")
+  const playButton = document.querySelector("#playButton")
+  const newGameButton = document.querySelector("#newGame")
   const playerScoreText = document.querySelector(".playerScore")
   const computerScoreText = document.querySelector(".computerScore")
-
-
   
-  selection.forEach(selector => {
+  // addEventListeners
+  selections.forEach((selector) => {
     selector.addEventListener("click", function (e) {
-      selection.forEach(selector =>{
-        selector.classList.remove("selected")
-      })
-      selector.classList.add("selected")
-      playerSelection = selector.id
-      makeSelectionText.textContent = `PLAY with ${playerSelection}, or make a choice:`
-      playButton.style.display = "inline-block"
-      playButton.classList.toggle("active")
+      changePlayerSelection(e.target)
+      makeSelectionText.textContent = `PLAY with ${e.target.id}, or make a choice:`
     })
   })
 
   playButton.addEventListener("click", () => {
-      playButton.classList.toggle("active")
-      playButton.style.display = "none"
-      makeSelectionText.textContent = "Make a choice:"
-      if (roundNumber < rounds) {
-        playRound(getPlayerSelection(), getComputerSelection())
-      }
+    if (!playerSelection) return
+    playRound(playerSelection, getComputerSelection())
+    checkGameOver()
   })
+  newGameButton.addEventListener("click", () => {
+    newGame()
+  })
+  
+  // Functions
+  function changePlayerSelection(selection) {
+    selections.forEach((selector) => {
+      if (selector.id === selection.id) {
+        playerSelection = selection.id
+        selector.classList.toggle("selected")
+      } else if (selector.classList.contains("selected")) {
+        selector.classList.remove("selected")
+      }
+    })
+  }
   
   function getComputerSelection() {
     const choice = Math.floor(Math.random() * 3)
@@ -45,55 +53,77 @@ function playGame(rounds = 5) {
     }
   }
   
-  function getPlayerSelection() {
-    return playerSelection
-  }
-  
   function playRound(playerSelection, computerSelection) {
-    const roundUL = document.querySelector(`.round${roundNumber+1}`)
+    const roundUL = document.querySelector(`.round${roundNumber}`)
     const roundListItem = document.createElement("li")
-    let roundText = roundListItem.textContent = ""
+    roundListItem.classList.add("result")
+    let roundResult
     if (playerSelection === computerSelection) {
-      roundUL.appendChild(roundListItem)
-      roundListItem.textContent = `TIE! Repeat Round ${roundNumber + 1}`
-      return
+      roundListItem.textContent = `TIE! ${playerSelection} vs ${computerSelection}`
     } else if (playerSelection === "rock") {
       if (computerSelection === "paper") {
-        roundText = "Paper covers Rock!"
-        roundListItem.style.color = "#E74C3C"
-        computerScore++
+        roundResult = "lose"
       } else {
-        roundText = "Rock smashes Scissors! +1"
-        roundListItem.style.color = "#27AE60"
-        playerScore++
+        roundResult = "win"
       }
     } else if (playerSelection === "paper") {
       if (computerSelection === "scissors") {
-        roundText = "Scissors cuts Paper!"
-        roundListItem.style.color = "#E74C3C"
-        computerScore++
+        roundResult = "lose"
       } else {
-        roundText = "Paper covers Rock! +1"
-        roundListItem.style.color = "#27AE60"
-        playerScore++
+        roundResult = "win"
       }
     } else {
       if (computerSelection === "rock") {
-        roundText = "Rock smashes Scissors!"
-        roundListItem.style.color = "#E74C3C"
-        computerScore++
+        roundResult = "lose"
       } else {
-        roundText = "Scissors cuts Paper! +1"
-        roundListItem.style.color = "#27AE60"
-        playerScore++
+        roundResult = "win"
       }
     }
+    if (roundResult === "win") {
+      roundListItem.textContent = `WIN! ${playerSelection} vs ${computerSelection}`
+      roundListItem.style.color = "#27AE60"
+      playerScore++
+      roundNumber++
+    } else if (roundResult === "lose") {
+      roundListItem.textContent = `LOSE! ${playerSelection} vs ${computerSelection}`
+      roundListItem.style.color = "#E74C3C"
+      computerScore++
+      roundNumber++  
+    }
     roundUL.appendChild(roundListItem)
-    roundListItem.textContent = roundText
     playerScoreText.textContent = `Player Score: ${playerScore}`
     computerScoreText.textContent = `Computer Score: ${computerScore}`
-    roundNumber++
   }
+
+  function checkGameOver() {
+    if (roundNumber > 5) {
+      playButton.style.display = "none"
+      newGameButton.style.display = "inline-block"
+      declareWinner()
+    }
+  }
+  function declareWinner () {
+    if (playerScore > computerScore) {
+      makeSelectionText.textContent = `WINNER! `
+    } else {
+      makeSelectionText.textContent = "You Lost! "
+    }
+    makeSelectionText.textContent += `${playerScore} to ${computerScore}`
+  }
+
+  function newGame() {
+    const results = Array.from(document.querySelectorAll(".result"))
+    results.forEach(result => {
+      result.remove()
+    })
+    makeSelectionText.textContent = "Make a choice:"
+    playButton.style.display = "inline-block"
+    newGameButton.style.display = "none"
+    playerSelection = null
+    playerScore = 0
+    computerScore = 0
+    roundNumber = 1
+    }
 }
 
 playGame()
